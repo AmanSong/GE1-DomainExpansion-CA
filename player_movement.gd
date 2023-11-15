e5e9259c9481c1e503d9e5fc8c053eea0fd0a5b9
Aman Song <amansong70@gmail.com>
2023-11-15 21:22:01 +0000

    improved player movement

--- player_movement.gd
+++ player_movement.gd
@@ -1,20 +1,56 @@
-extends RigidBody3D
+extends CharacterBody3D
 
-@export var speed = 10.0
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@export var current_speed = 5.0
+@export var walk_speed = 5.0
+@export var sprint_speed = 10.0
+@export var mouse_sensitivity = 0.25
+@onready var pov = $POV
+
+var is_jumping = false
+var jump_height = 5.0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var left_right = Input.get_axis("move_left", "move_right")
-	if abs(left_right) > 0:     
-		translate(Vector3.RIGHT * speed * left_right * delta)
-	
-	var for_back = Input.get_axis("move_backward", "move_forward")
-	if abs(for_back) > 0:     
-		translate(Vector3.FORWARD * speed * for_back * delta)
+	# Reset jump status when the character lands
+	if is_on_floor():
+		is_jumping = false
+
+# for camera movement
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
+		pov.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
+		pov.rotation.x = clamp(pov.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+  
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		is_jumping = true
+		velocity.y = sqrt(2.0 * gravity * jump_height)
+
+func _physics_process(delta):
+	#to add gravity
+	if not is_on_floor():
+		velocity.y -= gravity * delta
 		
+	# for sprinting or walking
+	if Input.is_action_pressed("sprint"):
+		current_speed = sprint_speed
+	else:
+		current_speed = walk_speed
+		
+	var input_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction = (transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
+
+	move_and_slide()
